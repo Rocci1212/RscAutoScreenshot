040055928165470afa3934e7a9409cf39718635c
Rocci <rocci1212@gmail.com>
2020-05-22 11:43:44 -0400

    update with configuration values

--- RscAutoScreenshot/RscAutoScreenshot.h
+++ RscAutoScreenshot/RscAutoScreenshot.h
@@ -4,10 +4,14 @@
 
 class RscAutoScreenshot : public BakkesMod::Plugin::BakkesModPlugin
 {
+private:
+	bool isEnabled;
+	int screenshotDelay;
+
+public:
 	virtual void onLoad();
 	virtual void onUnload();
 
 	void StatsScreen(std::string eventName);
 	void takeScreenshot();
-	void Render(CanvasWrapper canvas);
 };
